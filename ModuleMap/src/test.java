eff2df0c1123787ea2eea0ceb14a8281b3022a6f
DUT-Jiang <gaojiang@mail.dlut.edu.cn>
2019-07-11 17:33:02 +0800

    gengxin

--- ModuleMap/src/test.java
+++ ModuleMap/src/test.java
@@ -6,8 +6,8 @@ import org.model.SchemeMake.Units.point;
 
 public class test {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub;
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub;
 //		float x1 = (float)39.084105;
 //		float y1 = (float)121.817608;
 //		float x2 = (float)39.057184;
@@ -16,8 +16,8 @@ public class test {
 //		point src=new point(x1,y1);
 //		point dst=new point(x2,y2);
 //		Map.getAllJunction(src, dst);
-		point k = new EncodingReturn(EncodingRequest.SendRequest("北京市海淀区上地十街10号")).getLocation().toString();
-		System.out.println(k.toString2());
-	};
+//		String k = new EncodingReturn(EncodingRequest.SendRequest("北京市海淀区上地十街10号")).getLocation().toString();
+//		System.out.println(k.toString());
+//	};
 
 }
